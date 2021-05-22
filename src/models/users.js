822c8c6f9f6c9705c841640379e31d6e360c271f
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-05-22 20:10:59 -0300

    users' model created

--- src/models/users.js
+++ src/models/users.js
@@ -1,50 +1,26 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
-const uniqueValidator = require("mongoose-unique-validator");
-const mongoosePaginate = require("mongoose-paginate-v2");
-const ROLES = require("../constants/constants");
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../loaders/sequelize/sequelize");
 
-//the schema defines the structure of the doc i.e the user that will be persisted to the db i.e what attrs each user will have, besides the data type of every attr
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Name required."],
-    },
-    lastName: {
-      type: String,
-      required: [true, "Last name required."],
-    },
-    email: {
-      type: String,
-      required: [true, "Email required."],
-      //avoid that more than one user with a certain email can be created
-      unique: true,
-      index: true,
-    },
-    birthDate: Date,
-    password: {
-      type: String,
-      required: [true, "Password required."],
-    },
-    role: {
-      type: String,
-      required: true,
-      default: ROLES.USER_ROLE,
-      enum: [ROLES.USER_ROLE, ROLES.ADMIN_ROLE],
-    },
-    enable: {
-      type: Boolean,
-      required: true,
-      default: true,
-    },
+const User = sequelize.define("Users", {
+  // Model attributes are defined here
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  { timestamps: true }
-);
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  enable: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+});
 
-//use mongoose validator
-userSchema.plugin(uniqueValidator, { message: "already exists in the DB." });
-//add pagination
-userSchema.plugin(mongoosePaginate);
-
-module.exports = mongoose.model("users", userSchema); //the first param is the name of the collection in the db and the second, the schema that collection will asign to every doc persisted
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
