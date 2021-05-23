3a254875723aa30da8cc53d7d1c4b16be2614107
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-05-23 11:34:02 -0300

    uncomment previoulsy commented out lines in users' middlewares file

--- src/middlewares/users/userMiddlewares.js
+++ src/middlewares/users/userMiddlewares.js
@@ -1,36 +1,30 @@
 //File to validate the users' data i.e the attrs set in the user model, in every endpoint
 const { check } = require("express-validator");
 const AppError = require("../../handlers/AppError");
-// const { USER_ROLE, ADMIN_ROLE } = require("../../constants/constants");
+const { USER_ROLE, ADMIN_ROLE } = require("../../constants/constants");
 const { commonValidationResult } = require("../commonMiddlewares");
-// const { validJWT, hasRole } = require("../auth/authMiddlewares");
+const { validJWT, hasRole } = require("../auth/authMiddlewares");
 const UserServices = require("../../services/userServices");
 const userServices = new UserServices();
 
 //check if the entered id is a mongo id
 // const _isMongoID = check("id").isMongoId();
 
-//check if the name, last name, email and pass were passed through the body
+//check if the name, email and pass were passed through the body
 //check if the user's id was entered as a param in the endpoint
 const _idRequired = check("id", "ID is required").not().isEmpty();
 const _nameRequired = check("name", "Name is required").not().isEmpty();
-// // const _lastNameRequired = check("lastName", "Last name is required")
-// .not()
-// .isEmpty();
 const _emailRequired = check("email", "Email is required").not().isEmpty();
 const _passRequired = check("password", "Password is required").not().isEmpty();
 
-//check if the the email, the birth date and the assigned role passed have the correct format e.g username@email..., mm-dd-yyyy and user role respectively
+//check if the the email and the assigned role passed have the correct format e.g username@email..., mm-dd-yyyy and user role respectively
 const _isEmailValid = check("email", "Email is invalid").isEmail();
-// const _isBirthDateValid = check("birthDate", "Birth date is invalid")
-//   .optional()
-//   .isDate("MM-DD-YYYY");
-// const _isRoleValid = check("role")
-//   .optional()
-//   .custom(async (role = "") => {
-//     const ROLES = [USER_ROLE, ADMIN_ROLE];
-//     !ROLES.includes(role) && new AppError("Invalid role", 400);
-//   });
+const _isRoleValid = check("role")
+  .optional()
+  .custom(async (role = "") => {
+    const ROLES = [USER_ROLE, ADMIN_ROLE];
+    !ROLES.includes(role) && new AppError("Invalid role", 400);
+  });
 //updated email
 const _isOptionalMailValid = check("email", "Email is invalid")
   .optional()
@@ -55,57 +49,49 @@ const _optionalMailExists = check("email")
   });
 
 //validations for the /users GET endpoint
-const getAllRequestValidations = [
-  // validJWT,
-  commonValidationResult,
-];
+const getAllRequestValidations = [validJWT, commonValidationResult];
 
 //validations for the /users/:id GET endpoint
 const getByIdRequestValidations = [
-  commonValidationResult,
-  // validJWT,
+  validJWT,
   _idRequired,
-  // _isMongoID,
   _idExists,
+  commonValidationResult,
 ];
 
 //validations for the /users POST endpoint
 const postRequestValidations = [
-  commonValidationResult,
-  // validJWT,
+  validJWT,
+  hasRole(ADMIN_ROLE),
   _nameRequired,
-  // _lastNameRequired,
   _emailRequired,
   _isEmailValid,
   _emailExists,
   _passRequired,
-  // _isRoleValid,
-  // _isBirthDateValid,
-  // hasRole(ADMIN_ROLE),
+  _isRoleValid,
+  commonValidationResult,
 ];
 
 //validations for the /users/:id PUT endpoint
 const putRequestValidations = [
-  commonValidationResult,
-  // validJWT,
+  validJWT,
+  hasRole(ADMIN_ROLE),
   _idRequired,
-  // _isMongoID,
   _idExists,
   _isOptionalMailValid,
   _optionalMailExists,
-  // _isBirthDateValid,
-  // _isRoleValid,
-  // hasRole(ADMIN_ROLE),
+  _isRoleValid,
+
+  commonValidationResult,
 ];
 
 //validations for the /users/:id DELETE endpoint
 const deleteRequestValidations = [
-  commonValidationResult,
-  // validJWT,
+  validJWT,
+  hasRole(ADMIN_ROLE),
   _idRequired,
-  // _isMongoID,
   _idExists,
-  // hasRole(ADMIN_ROLE),
+  commonValidationResult,
 ];
 
 module.exports = {
