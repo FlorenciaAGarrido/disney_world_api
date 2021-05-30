98a191b38e13b128d2ac55df6a601153f8e3c07d
Joaquín Villanueva <villanuevajoaquin94@gmail.com>
2021-05-30 13:54:35 -0300

    movie services file updated: the genre and the content type repos were implemented here

--- src/services/MovieServices.js
+++ src/services/MovieServices.js
@@ -1,22 +1,48 @@
 //in this layer the business logic takes place
 const MovieRepository = require("../repositories/MovieRepository");
+const GenreRepository = require("../repositories/GenreRepositpory");
+const ContentTypeRepository = require("../repositories/ContentTypeRepository");
 
 class MovieServices {
   constructor() {
-    this.repo = new MovieRepository();
+    this.movieRepo = new MovieRepository();
+    this.genreRepo = new GenreRepository();
+    this.contentTypeRepo = new ContentTypeRepository();
   }
 
-  getAll = async () => await this.repo.findAll();
+  getAll = async () => await this.movieRepo.findAll();
 
-  getByID = async (id) => await this.repo.findByID(id);
+  getByID = async (id) => await this.movieRepo.findByID(id);
 
-  getByTitle = async (title) => await this.repo.findByTitle(title);
+  getByTitle = async (title) => await this.movieRepo.findByTitle(title);
 
-  create = async (movie) => await this.repo.save(movie);
+  create = async (movie) => {
+    const genre = await this.genreRepo.findByDescription(movie.genre);
+    const contentType = await this.contentTypeRepo.findByDescription(
+      movie.contentType
+    );
 
-  edit = async (id, movie) => await this.repo.update(id, movie);
+    movie.genreID = genre.id;
+    movie.contentTypeID = contentType.id;
 
-  delete = async (id) => await this.repo.remove(id);
+    return await this.movieRepo.save(movie);
+  };
+
+  edit = async (id, movie) => {
+    if (movie.genre) {
+      const genre = await this.genreRepo.findByDescription(movie.genre);
+      const contentType = await this.contentTypeRepo.findByDescription(
+        movie.contentType
+      );
+
+      movie.genreID = genre.id;
+      movie.contentTypeID = contentType.id;
+    }
+
+    return await this.movieRepo.update(id, movie);
+  };
+
+  delete = async (id) => await this.movieRepo.remove(id);
 }
 
 module.exports = MovieServices;
